6e744ddd426f8ef393cfb082cd90e5910f79ea16
Lord Rabban Ashitey <lordrabbanashitey@gmail.com>
2024-04-08 10:55:59 +0000

    Changed the data types of some variables

--- Variables.py
+++ Variables.py
@@ -1,6 +1,6 @@
 # Create variables 
-# Change from one data type to the other
 # Display the data type of a variable 
+# Change from one data type to the other
 # Take inputs from a user 
 # Display outputs to a user 
 
@@ -18,3 +18,9 @@ print (f"The variable BMI is of data type {type(BMI)}")
 print (f"The variable Hobbies is of datat type {type(Hobbies)}")
 print (f"The variable Favorites is of data type {type(Favorites)}")
 print (f"The varible Pairs is of data type {type(Pairs)}")
+Age1 = str(Age)
+print(f"Age is now a {type(Age1)}")
+Age2 = float(Age)
+print (f"Age is now a type {type(Age2)}")
+Legal1 = str(Legal)
+print(f"Legal is now a {type(Legal1)}")
