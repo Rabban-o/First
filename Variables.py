a5c173564f8fc5b6d61de86a356254f63b44b3c3
Lord Rabban Ashitey <lordrabbanashitey@gmail.com>
2024-04-08 08:03:42 +0000

    Created Variables successfully

---
+++ Variables.py
@@ -0,0 +1,20 @@
+# Create variables 
+# Change from one data type to the other
+# Display the data type of a variable 
+# Take inputs from a user 
+# Display outputs to a user 
+
+Name = ""
+Age = 0
+Legal = False
+BMI = 35.67
+Hobbies = ["Drawing", "Painting", "Grafitti"]
+Favorites = {}
+Pairs = ()
+print (f"The variable Name is of data type {type(Name)}")
+print (f"The variable Age is of data type {type(Age)}")
+print (f"The variable Legal is of data type {type(Legal)}")
+print (f"The variable BMI is of data type {type(BMI)}")
+print (f"The variable Hobbies is of datat type {type(Hobbies)}")
+print (f"The variable Favorites is of data type {type(Favorites)}")
+print (f"The varible Pairs is of data type {type(Pairs)}")
