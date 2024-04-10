cca1745a4bf94f3af04c74fede27a130991d4a8e
Lord Rabban Ashitey <lordrabbanashitey@gmail.com>
2024-04-10 13:30:28 +0000

    Manipulated a string

---
+++ Cipher.py
@@ -0,0 +1,37 @@
+# Trying to learn string manipulation
+# Create a function for Caesar Cipher
+# Decrypt the cipher
+# work on Vigenere Cipher 
+#Decrypt the vigenere cipher
+
+myString = 'This is a string value'
+
+# indexing strings
+print(myString[12])
+print(myString[-1])
+
+# slicing through a string
+print(myString[3:8])
+print (myString[12:-1])
+
+# using some string methods
+print(myString.split())
+print(myString.capitalize())
+print(myString.upper())
+print(myString.isalpha())
+print(myString.isspace())
+print(myString.find('r'))
+
+# Add ons that I do not know how to describe
+print(len(myString))
+print(list(myString))
+
+
+## Hill Cipher 
+## Hill cipher takes the number equivalent of a letter and adds 3 to it to get encrypt it
+#def Hill_Cipher(word):
+ #   reference = 'abcdefghijklmnopqrstuvwxyz'
+  #  method = input('Encrypt or Decrypt? Type E for encrypt and D for decrypt').upper()
+   # word = input('Enter the text to be decrypted')
+    
+     
